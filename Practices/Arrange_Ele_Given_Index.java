b92b78a2dd739603688e41e9a7b4cae48788e29f
Siva M <145832989+Sivamatheshwaran4@users.noreply.github.com>
2024-07-31 11:05:38 +0530

    Update Arrange_Ele_Given_Index.java

--- Practices/Arrange_Ele_Given_Index.java
+++ Practices/Arrange_Ele_Given_Index.java
@@ -5,7 +5,7 @@ public class Arrange_Ele_Given_Index {
 	    	 String str="Raja";
 	         int []index={1,0,2,3};
 	       StringBuilder sb = new StringBuilder();
-	        for(int i=0; i<str.length(); i++) {
+	        for(int i=0; i<index.length; i++) {
 	        	int k = index[i];
 	        	sb.append(str.charAt(k));
 	        }
