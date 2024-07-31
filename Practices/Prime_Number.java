2df5717237da5023b6ae6fef7c4ae33a403dbe8e
Siva M <145832989+Sivamatheshwaran4@users.noreply.github.com>
2024-07-31 11:06:36 +0530

    Update Prime_Number.java

--- Practices/Prime_Number.java
+++ Practices/Prime_Number.java
@@ -2,23 +2,27 @@ package Practices;
 
 public class Prime_Number {
 
-	public static void main(String[] args) {
-		int n=3 , m=0 , flag=0;
-		m=n/2;
-		if(n==0 || n==1) {
-			flag =1;
-			System.out.println(n + " Is not prime Num");
-		}
-		else {
-			for (int i = 2; i <= m; i++) {
-				if (n % i == 0) {
-					flag = 0;
-					break;
-				}
-			}
-		}
-		if(flag==0)
-			System.out.println(n + " Is prime Num");
-	}
+public static void main(String[] args) {
+        int n = 4;
+        int flag = 1;
+
+        if (n == 0 || n == 1) {
+            flag = 0;
+            System.out.println(n + " is not a prime number");
+        } else {
+            for (int i = 2; i <= n / 2; i++) {
+                if (n % i == 0) {
+                    flag = 0;
+                    break;
+                }
+            }
+        }
+
+        if (flag == 1) {
+            System.out.println(n + " is a prime number");
+        } else if (n > 1) {
+            System.out.println(n + " is not a prime number");
+        }
+    }
 
 }
